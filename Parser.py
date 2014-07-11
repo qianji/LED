c3e82e7043476f7e971ba56433c642adfb70c2b6
qianji <qianji.zheng@gmail.com>
2014-07-11 16:28:33 -0500

    Refactor the functions. Delete duplicate and unused functions

--- Parser.py
+++ Parser.py
@@ -1,7 +1,6 @@
 ######################################################################
 # Parser
-
-# Qianji Zheng
+# Qianji Zheng, Texas Tech University
 # July 2014
 ######################################################################
 
@@ -19,13 +18,13 @@ If *AST* is a node r, it represents an abstract syntax tree with r as its root n
 A *var* or an *identifier* is a nonempty string of letters and digits beginning with a letter.
 A *num* is a nonempty string of digits
 '''
-''''''''''''''''''''''''''''''''''''''''''''
-# All the functions in this file that starts with the word "parse" have the same signature as follows:
+'''
+# All the functions in this file that starts with the word "parse" have the same signature as follows if their function signatures are not provided:
 # list(string) -> AST * bool
-# if S is a list of string then parse**(S) = (tree,True), where tree is the abstract syntax tree of S, if S complies to the corresponding rule of the grammar
+# If S is a list of string then parse**(S) = (tree,True), where tree is the abstract syntax tree of S, if S complies to the corresponding rule of the grammar
 # otherwise parse**(S) = (None,False).
-# Each rule is given right before the funtion definiton
-''''''''''''''''''''''''''''''''''''''''''''
+# Each rule is given right before the function definition
+'''
 
 # rule: funcBody -> Expression | conditional
 def parseFuncBody(S):
@@ -84,6 +83,7 @@ def parseIfClause(S):
     return (None,False)    
 
 '''
+# helper function
 # str * list<str> -> int
 # If C is a string and S is a list of string, lastIndex(C, S) is the index of the last C in S if there is at least C,
 # otherwise lastIndex(C, S) = 0
@@ -107,9 +107,11 @@ def parseExpression(S):
         return (tree1,True)
     return (None,False)
 
+# rule: Sentence -> S6
 def parseSentence(S):
     return parseS6(S)
 
+# rule: S6 -> S5    |   S5 <=>  S6
 def parseS6(S):
     for i in range(len(S)):
         if S[i]=='<=>':
@@ -122,6 +124,7 @@ def parseS6(S):
         return (tree,True)    
     return (None,False)
 
+# rule: S5  ->   S4    |   S4 =>  S5
 def parseS5(S):
     for i in range(len(S)):
         if S[i]=='=>':
@@ -134,6 +137,7 @@ def parseS5(S):
         return (tree,True)    
     return (None,False)
 
+# rule: S4  > S3     |   S4  or  S3
 def parseS4(S):
     for i in range(len(S)):
         if S[i]=='or':
@@ -146,6 +150,7 @@ def parseS4(S):
         return (tree,True)    
     return (None,False)
 
+# rule: S3  ->  S2     |   S3  &  S2
 def parseS3(S):
     for i in range(len(S)):
         if S[i]=='&':
@@ -158,6 +163,7 @@ def parseS3(S):
         return (tree,True)    
     return (None,False)
 
+#rule: S2  ->  S0   |  ~ S2
 def parseS2(S):
     if len(S)==0:
         return (None,False)
@@ -170,6 +176,7 @@ def parseS2(S):
         return (tree,True)    
     return (None,False)
 
+#rule S0  ->   term   infpred   term 
 InfpredS0 = ['=','<','>','<=','>=','in','subeq']
 def parseS0(S):
     if len(S)==0: return(None,False)
@@ -190,6 +197,7 @@ def parseS0(S):
         if flag: return (tree,True)
     return (None,False)      
 
+#rule: term  ->   T4  
 def parseTerm(S):
     return parseT4(S)
 
@@ -223,6 +231,7 @@ def parseT3(S):
         return (tree,True)    
     return (None,False)
 
+# rule: T2   ->  T1   |  Prefix2  T2
 PrefixT2 = ['+','-']
 def parseT2(S):
     if len(S)==0:
@@ -237,7 +246,7 @@ def parseT2(S):
         return (tree,True)    
     return (None,False)    
 
-# rule: T1 -> T0 | vector [term]  | T0  ^  T1
+# rule: T1 -> T0 | T1 [term]  | T0  ^  T2
 InfixT1 = ['^']
 def parseT1(S):
     if len(S)==0:
@@ -253,7 +262,7 @@ def parseT1(S):
             t2,f2 = parseTerm(S[lb+1:len(S)-1])
             if f1 and f2:
                 return (('sub',[t1,t2]),True)
-    # T0 ^ T1
+    # T0 ^ T2
     for i in range(len(S)):
         for infix in InfixT1:
             if S[i]==infix:
@@ -267,12 +276,8 @@ def parseT1(S):
         return (tree,True)    
     return (None,False) 
     
-'''
-# list(string) -> AST * bool
-# if S is a list of string then parseT0(S) = (tree,True), where tree is the abstract syntax tree of S, if S complies to rule T0 of the grammar
-# otherwise parseE0(S) = (None,False).
-#rule 0: T0 -> numeral | identifier | (term) | pipe term pipe | floor(term) | ceil(term) | prefun(terms) | vector | set | tuple | Pow (vector) | choose(vector)
-'''
+#rule 0: T0 -> numeral | identifier | (term) | pipe term pipe | floor(term) | ceil(term) | prefun(terms) | 
+#              vector | set | tuple | Pow (vector) | choose(vector)| { term .. term }
 def parseT0(S):
     if len(S)==0:
         return (None,False)
@@ -310,10 +315,10 @@ def parseT0(S):
             if isNumeral(S[0]): return (eval(S[0]),True)
             # identifier
             if isIdentifier(S[0]): return (S[0],True)
-         # parse vector   
+        # parse vector   
         (tree,flag) = parseVector(S)
         if flag: return (tree,True)
-         # parse set   
+        # parse set   
         (tree,flag) = parseSet(S)
         if flag: return (tree,True)
         # parse tuple
@@ -322,12 +327,8 @@ def parseT0(S):
     except IndexError:
         return(None,False) 
     return (None,False)
-'''
-# list(string) -> AST * bool
-# if S is a list of string then parseUserDefinedFun(S) = (tree,True), where tree is the abstract syntax tree of S, if S complies to the following rule of the grammar
-# otherwise parseUserDefinedFun(S) = (None,False).
+
 # rule: T0 -> prefun ( terms )
-'''
 def parseUserDefinedFun(S):
     try:
         if S[len(S)-1] ==')':
@@ -343,24 +344,15 @@ def parseUserDefinedFun(S):
     except ValueError:
         return(None,False)         
     return (None,False)
-'''
-# list(string) -> AST * bool
-# if S is a list of string then parsePrefun(S) = (tree,True), where tree is the abstract syntax tree of S, if S complies to the following rule of the grammar
-# otherwise parsePrefun(S) = (None,False).
+
 # rule: prefun -> identifier
-'''
 def parsePrefun(S):
     if(len(S)==1):
         if isIdentifier(S[0]):
             return (S[0],True)
     return (None,False)
 
-'''
-# list(string) -> AST * bool
-# if S is a list of string then parseTerms(S) = (tree,True), where tree is the abstract syntax tree of S, if S complies to the following rule of the grammar
-# otherwise parseTerms(S) = (None,False).
 # rule: terms -> term | term , terms
-'''
 def parseTerms(S):
     for i in range(len(S)):
         if S[i]==',':
@@ -377,6 +369,7 @@ def parseTerms(S):
     return (None,False)     
      
 '''
+# helper function
 # str -> bool
 # isIdentifier(S) iff S is an *identifier*
 '''
@@ -387,6 +380,7 @@ def isIdentifier(S):
     return True
 
 '''
+# helper function
 # string -> bool
 # isNumeral(S) iff S is a *numeral*
 '''
@@ -394,11 +388,11 @@ def isNumeral(S):
     for c in S:
         if not (digit(c) or c=='.'): return False
     return True
-
     
 '''
+# helper function
 #  AST -> bool
-If T is an AST, then hasIdentifier(T) iff there is at least one indentifier in T
+# If T is an AST, then hasIdentifier(T) iff there is at least one indentifier in T
 '''    
 def hasIdentifier(T):
     if isScalar(T): return False
@@ -407,17 +401,7 @@ def hasIdentifier(T):
     for i in X:
         if hasIdentifier(i):
             return True
-
     return False
-    
-'''
-value(S) is used for unit testing
-'''
-def value(S):
-    (S,f1) = tokens(S)
-    tree,f2 = parseExpression(S)
-    v = val(tree)
-    return v
 
 '''
 string -> dict * bool
@@ -428,7 +412,7 @@ For example, the following program f(x) := x^2  g(x,y) := y+2*x would be represe
 ('g',2):(['x','y'],('+',['y',('*',[2,'x'])])) } 
 '''
 '''
-# Dfn -> funcDef | relDef
+# rule: Dfn -> funcDef | relDef
 '''
 def parseDfn(S):
     program2,flag2 = parseFuncDef(S)
@@ -446,7 +430,7 @@ otherwise parseFuncDef(S) = (None,False)
 For example, the following program f(x) := x^2  g(x,y) := y+2*x would be represented by the following dictionary: 
 {('f',1):(['x'],('^',['x',2])) , 
 ('g',2):(['x','y'],('+',['y',('*',[2,'x'])])) } 
-# funcDef -> identifier ( vars )  :=   funcBody
+#rule: funcDef -> identifier ( vars )  :=   funcBody
 '''
 def parseFuncDef(S):
     program = {}
@@ -461,15 +445,7 @@ def parseFuncDef(S):
                 return(program,True)     
     return (None,False)     
 
-'''
-string -> dict * bool
-If S is a string of the function definitions of relDef defined in LED, then parseRelDef(S) = (dict,True), where dict is a dictionary 
-otherwise parseRelDef(S) = (None,False)
-'''
-
-'''
 # relDef -> identifier ( vars ) iff   sentence
-'''
 def parseRelDef(S):
     program = {}
     for i in range(len(S)):
@@ -482,46 +458,6 @@ def parseRelDef(S):
                 program[(fName,paramNumber)] = (fParams,t2)
                 return(program,True)     
     return (None,False) 
-'''
-###
-This is the candidate program using the format of keyword 'let'. To be deleted
-###
-string -> dict * bool
-If S is string of the function definitions of LED, then parseProgram(S) = (dict,True), where dict is a dictionary 
-otherwise parseProgram(S) = (None,False)
-For example, the following program let f(x) = x^2 let g(x,y) = y+2*x would be represented by the following dictionary: 
-{('f',1):(['x'],('^',['x',2])) , 
-('g',2):(['x','y'],('+',['y',('*',[2,'x'])])) } 
-'''
-def parseProgramLet(S):
-    program = {}
-    while(len(S)>0):
-        #find the first 'let' in S
-        firstLetIndex = S.index('let')
-        # find the first '=' immediately after the first 'let'
-        firstEqualIndex = S.index('=')
-        # find the second 'let' in S 
-        try:
-            secondLetIndex = S[firstLetIndex+1:].index('let') + firstLetIndex+1
-        except ValueError:
-            #if cannot find the second 'let', then reach the last function definition of the program
-            secondLetIndex = len(S)
-        # parse the expression in between the first '=' and the second 'let'
-        #tree,flag = parseExpression(S[firstEqualIndex+1:secondLetIndex])
-        tree,flag = parseFuncBody(S[firstEqualIndex+1:secondLetIndex]) 
-        # parse the left hand side of the function definition between the first 'let' and the first '='
-        ((fName,fParams),flag2) = parseLHS(S[firstLetIndex+1:firstEqualIndex])
-        if flag and flag2:
-            paramNumber = len(fParams)
-            #put the content in the dictionary
-            program[(fName,paramNumber)] = (fParams,tree)
-            # move to next function definition
-            S= S[secondLetIndex:len(S)]
-        else:
-            # print the message
-            return (None,False)
-    return (program,True)
-
 
 '''
 # string -> list< list<string> >
@@ -556,6 +492,8 @@ def parseProgram(S):
     return allFunctions
 
 '''
+# helper function
+# char * int * str -> int
 firstIndexBack(C, secondeI, S) searches backward from index of secondI of S to find the first C in S
 '''        
 def firstIndexBack(C,secondI,S):
@@ -563,7 +501,9 @@ def firstIndexBack(C,secondI,S):
         if S[i]==C:
             return i
     return None
+
 '''
+# helper function
 # lsit<str> * list<str> -> int
 # If Cs is a list of string and S is a list of string, firstIndex(C, S) is the first index of one of the member of C in S,
 # otherwise firstIndex(Cs, S) = len(S) 
@@ -578,7 +518,7 @@ def firstIndex(Cs, S):
     return None
 
 '''
-list<str> -> str * list<str>
+list<str> -> (str * list<str>) * bool
 If S is a list of string that comply to the format of the left hand side of the function definition, 
 then parseLHS(S) = ((fName,fParams),True), where fname is a string and fParams is a list of strings
 otherwise, parseLHS(S) = (None,False)
@@ -592,6 +532,7 @@ def parseLHS(S):
     return (None,False)
 
 '''
+# helper function
 list<str> -> list<str>
 If S is a list of string that comply to the format of the parameters of the function definition, 
 then params(S) is a list of parameters of the function definition
@@ -601,18 +542,15 @@ def params(S):
     return [p for p in S if not p==',']
 
 '''
-
-'''
-'''
 # Program -> set
 # If P is a program, then definedFuns(P) is a set of function named defined in P.
 '''
 def definedFuns(P):
     return {Def[0] for Def in P}
 
+# helper function for removeComments
 # canPushParser(S,state) iff, in the current state S with the current stack stk,
 # stk+[S[0]] is the beginning of a token.
-
 def canPushParser(S,state):
     if S==[]: return False
     if state =='success': return False
@@ -628,10 +566,11 @@ def canPushParser(S,state):
     if state =='w1' : return c=='d' or white(c)
     if state == 'w2' : return 'w2'
     return False
-
+'''
+# helper function for removeComments
 # If canpushParser(S,state) and c=S[0], the newState(state,c) is the
 # state resulting from pushing c onto the stack.
-
+'''
 def newStateParser(state,c):
     if state == 'empty':
         return 'lb' if c=='{' else \
@@ -643,12 +582,6 @@ def newStateParser(state,c):
     if state == 's': return 'w2' if white(c) else 'spec1'
     if state =='w2': return 'w2' if white(c) else 'success'
 
-'''
-# string -> list< list<string> >
-# if S is a string of the program that has f1, f2, ... fn functions and 0 or more comments, then parseProgram(S)=[L1, L2, ... Ln], where L1... Ln 
-# are lists of corresponding function to f1, f2,... fn.
-'''
-        
 '''
 # If S is a string, then firstFuncHead(S)=(s,e), where s is the first index of { followed by 0 or more white space 
 # and followed by 'def' or 'defs' and 0 or more white spaces and e is the first non white space character after 'def' or 'defs'
@@ -669,6 +602,7 @@ def firstFuncHead(S):
     return None
 
 '''
+str -> str
 If S is a string, then removeComments(S) is a string that does not have any comments in S
 '''
 def removeComments(FS):
@@ -687,6 +621,7 @@ def removeComments(FS):
             S = S[firstRb+1:]
     return allFunctionsText
 '''
+# helper function for removeComments
 This is a function definition: {def f(x) = {x}+{x+2} }
 closeParentesis(S) finds the first } that does not match any { in S
 '''
@@ -721,7 +656,7 @@ def closeParentesis(S):
         index+=1 
     return None    
             
-# vector -> <  >  | <  terms >
+# rule: vector -> <  >  | <  terms >
 def parseVector(S):
     if len(S)<2:
         return (None,False)
@@ -740,7 +675,7 @@ def parseVector(S):
  
     return (None,False)
     
-# set -> {  }  | { terms }
+# rule: set -> {  }  | { terms }
 def parseSet(S):
     if len(S)<2:
         return (None,False)
@@ -760,7 +695,7 @@ def parseSet(S):
     return (None,False)
 
     
-# set -> | ( terms )
+# rule: set -> | ( terms )
 def parseTuple(S):
     if len(S)<2:
         return (None,False)
@@ -774,26 +709,6 @@ def parseTuple(S):
                 return(('tuple',[tree]),True)
  
     return (None,False)
-
-def main():  
-    while True:
-        i = input('please input your expression:')
-        (S,f1) = tokens(i)
-        if f1: 
-            tree,f2 = parseExpression(S)
-            if f2:
-                print('The abstract syntax tree of your expression is',tree)
-                '''
-                if hasIdentifier(tree):    
-                    x= eval(input('please input a number for x: '))
-                    print(val(sub(x,'x',tree)))
-                else:
-                    print(val(tree))
-                '''
-            else:
-                print("Wrong input format, cannot parse the input")
-        else:
-            print("cannot tokenize the input")
-#main()      
+     
 #print(parseTerms(tokens('2+2,3,x2')[0]))
 #print(isIdentifier('2'))
