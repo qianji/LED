c7df8e2c87fd0daa35e09d8192f3dc54a3cb0d5d
qianji <qianji.zheng@gmail.com>
2014-07-16 16:37:18 -0500

    no message

--- GlobalVars.py
+++ GlobalVars.py
@@ -4,5 +4,8 @@ July 2014
 
 This file contains all the shared global variables in LED
 '''
-
+# The definition f(x1,...,xn):= E is represented by the Python dictionary entry ('f',n) : ([x1,...,xn],E).
+# For example, the following program f(x) := x^2  g(x,y) := y+2*x would be represented by the following dictionary: 
+# {('f',1):(['x'],('^',['x',2])) , 
+# ('g',2):(['x','y'],('+',['y',('*',[2,'x'])])) } 
 Program={}
